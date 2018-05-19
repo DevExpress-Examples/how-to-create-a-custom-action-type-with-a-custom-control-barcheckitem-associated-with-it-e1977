8e52a43f8723947119fa31947524d50e23908511
DevExpressExampleBot <DevExpressExampleBot@users.noreply.github.com>
2018-05-20 02:47:26 +0300

    initial commit

---
+++ CS/Solution5.Module/CheckableSimpleAction.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using DevExpress.ExpressApp.Actions;
+using System.ComponentModel;
+using DevExpress.ExpressApp;
+using DevExpress.Persistent.Base;
+
+namespace Solution5.Module
+{
+    public class CheckableSimpleAction : SimpleAction
+    {
+        public CheckableSimpleAction(IContainer container) : base(container) { }
+        public CheckableSimpleAction(Controller owner, string id, PredefinedCategory category) : base(owner, id, category) { }
+
+        private bool _Checked = false;
+        public bool Checked
+        {
+            get
+            {
+                return _Checked;
+            }
+            set
+            {
+                if (_Checked != value)
+                {
+                    _Checked = value;
+                    DoCheckedStateChanged();
+                    if (Enabled.ResultValue && Active.ResultValue)
+                    {
+                        DoExecute();
+                    }
+                }
+            }
+        }
+
+        private void DoCheckedStateChanged()
+        {
+            if (CheckedStateChanged != null)
+            {
+                CheckedStateChanged(this, new EventArgs());
+            }
+        }
+
+        public event EventHandler CheckedStateChanged;
+    }
+}
---
+++ CS/Solution5/Solution5.Module/Module.cs
@@ -0,0 +1,17 @@
+using System;
+using System.Collections.Generic;
+
+using DevExpress.ExpressApp;
+using System.Reflection;
+
+
+namespace Solution5.Module
+{
+    public sealed partial class Solution5Module : ModuleBase
+    {
+        public Solution5Module()
+        {
+            InitializeComponent();
+        }
+    }
+}
